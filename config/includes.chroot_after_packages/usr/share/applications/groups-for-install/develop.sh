faa9c196be980ebcba694bbc22d7282b9526756d
Jose Francisco Gonzalez <jfgs1609@gmail.com>
2025-05-31 19:38:43 +0200

    Commit

---
+++ config/includes.chroot_after_packages/usr/share/applications/groups-for-install/develop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install the Flatpak applications
+flatpak install -y flathub "org.gnome.Builder"
+flatpak install -y flathub "com.visualstudio.code"
+flatpak install -y flathub "io.dbeaver.DBeaverCommunity"
+flatpak install -y flathub "com.jetbrains.IntelliJ-IDEA-Community"
+
+SCHEMA="org.gnome.desktop.app-folders"
+KEY="folder-children"
+
+STATUS=$(gsettings get ${SCHEMA} ${KEY})
+
+# Optional proof, whether value to add already exsists
+if [[ $STATUS == *"'Develop'"* ]]; then
+    echo "Develop folder already exists."
+else
+    gsettings set ${SCHEMA} ${KEY} "${STATUS%]*}, 'Develop']"
+    gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Develop/ name 'Develop.directory'
+    gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Develop/ apps "['org.gnome.Builder.desktop', 'com.visualstudio.code.desktop', 'io.dbeaver.DBeaverCommunity.desktop', 'com.jetbrains.IntelliJ-IDEA-Community.desktop']"
+    echo "Added Develop folder."
+fi
--- config/includes.chroot_after_packages/usr/share/applications/groups-for-install/games.sh
+++ config/includes.chroot_after_packages/usr/share/applications/groups-for-install/games.sh
@@ -1,4 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-# Install the Flatpak application
+# Install packages for games
+sudo apt install -y steam-devices
+
+# Install the Flatpak applications
 flatpak install -y flathub "com.valvesoftware.Steam"
+flatpak install -y flathub "org.gnome.Aisleriot"
+
+SCHEMA="org.gnome.desktop.app-folders"
+KEY="folder-children"
+
+STATUS=$(gsettings get ${SCHEMA} ${KEY})
+
+# Optional proof, whether value to add already exsists
+if [[ $STATUS == *"'Games'"* ]]; then
+    echo "Games folder already exists."
+else
+    gsettings set ${SCHEMA} ${KEY} "${STATUS%]*}, 'Games']"
+    gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Games/ name 'Games.directory'
+    gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Games/ apps "['org.gnome.Aisleriot.desktop', 'com.valvesoftware.Steam.desktop']"
+    echo "Added Games folder."
+fi
